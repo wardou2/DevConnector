652de7610c44eb1229c3f2c04265e8e0df3c881a
wardou2 <ward92@gmail.com>
2019-09-04 20:31:03 -0700

    bcrypt user password

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const gravatar = require('gravatar')
+const bcrypt = require('bcryptjs')
 const { check, validationResult } = require('express-validator')
 const User = require('../../models/User')
 
@@ -24,7 +25,7 @@ router.post('/', [
             let user = await User.findOne({ email })
 
             if(user) {
-                res.stats(400).json({ errors: [ { msg: 'User already exists'} ] })
+                return res.status(400).json({ errors: [ { msg: 'User already exists'} ] })
             }
 
             // Get users gravatar
@@ -34,10 +35,22 @@ router.post('/', [
                 d: 'mm'
             })
 
+            user = new User({ 
+                name, 
+                email,
+                avatar,
+                password
+            })
+
             // Encrypt password
+            const salt = await bcrypt.genSalt(10)
+
+            user.password = await bcrypt.hash(password, salt)
+
+            await user.save()
     
             // Return JSON web token
-            res.send('User route')
+            res.send('User registered')
 
         } catch(err) {
             console.error(err.message)
