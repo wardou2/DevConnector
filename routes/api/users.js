61ee4751787fa9a1db2d845d23844ba45ac36e41
wardou2 <ward92@gmail.com>
2019-09-11 13:25:34 -0700

    jwt token

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,7 +2,9 @@ const express = require('express')
 const router = express.Router()
 const gravatar = require('gravatar')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 const { check, validationResult } = require('express-validator')
+const config = require('config')
 const User = require('../../models/User')
 
 // @route   POST api/users
@@ -49,9 +51,21 @@ router.post('/', [
 
             await user.save()
     
-            // Return JSON web token
-            res.send('User registered')
+            const payload = {
+                user: {
+                    id: user.id
+                }
+            }
 
+            jwt.sign(
+                payload, 
+                config.get('jwtSecret'),
+                { expiresIn: 3600000 },
+                (err, token) => {
+                    if (err) throw err
+                    res.json({ token })
+                }
+            )
         } catch(err) {
             console.error(err.message)
             res.status(500).send('Server Error')
