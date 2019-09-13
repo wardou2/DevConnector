28f93f6aaf138d324bb0e9faa80b8b1d5ffa33bd
wardou2 <ward92@gmail.com>
2019-09-12 21:58:21 -0700

    profile route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router()
+const auth = require('../../middleware/auth')
 
-// @route   GET api/profiles
-// @desc    Test route
-// @access  Public
+// @route   GET api/profile/me
+// @desc    Get current user's profile
+// @access  Private
 router.get('/', (req, res) => res.send('profile routes'))
 
 module.exports = router
