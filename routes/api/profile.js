6cdc608606bdd042af75a9fb8a332763dd89b01e
wardou2 <ward92@gmail.com>
2019-09-19 23:26:49 -0700

    new

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -107,7 +107,6 @@ async (req, res) => {
 // @route   GET api/profile
 // @desc    Get all profiles
 // @access  Public
-
 router.get('/', async (req, res) => {
     try {
         const profiles = await Profile.find().populate('user', ['name', 'avatar'])
@@ -121,7 +120,6 @@ router.get('/', async (req, res) => {
 // @route   GET api/profile/user/:user_id
 // @desc    Get profile by user id
 // @access  Public
-
 router.get('/user/:user_id', async (req, res) => {
     try {
         const profile = await Profile.findOne({ user: req.params.user_id }).populate('user', ['name', 'avatar'])
@@ -140,7 +138,6 @@ router.get('/user/:user_id', async (req, res) => {
 // @route   DELETE api/profile
 // @desc    delete profile, user & posts
 // @access  Private
-
 router.delete('/', auth, async (req, res) => {
     try {
         // @todo: remove user's posts
@@ -150,6 +147,36 @@ router.delete('/', auth, async (req, res) => {
         // Remove user
         await User.findOneAndRemove({ _id: req.user.id })
         res.json({ msg: "User removed"})
+    } catch(err) {
+        console.error(err.message)
+        res.status(500).send("Server Error")
+    }
+})
+
+// @route   PUT api/profile/experience
+// @desc    Add profile experience
+// @access  Private
+
+router.put('/experience', [auth, [
+    check('title', 'Title is required').not().isEmpty(),
+    check('company', 'Company is required').not().isEmpty(),
+    check('from', 'From date is required').not().isEmpty(),
+]], async (req, res) => {
+    const errors = validationResult(req)
+    if(!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array()})
+    }
+
+    const { title, company, location, from, to, current, description } = req.body
+
+    const newExp = {title, company, location, from, to, current, description}
+
+    try {
+        const profile = await Profile.findOne({ user: req.user.id })
+
+        profile.experience.unshift(newExp)
+        await profile.save()
+        res.json(profile)
     } catch(err) {
         console.error(err.message)
         res.status(500).send("Server Error")
